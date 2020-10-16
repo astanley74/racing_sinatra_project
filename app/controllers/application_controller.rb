6b4bfba79295c43e52876a24e14d21b96c4a7fe4
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-10-16 14:24:39 -0400

    added helper methods and changed Owner model to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,15 @@ class ApplicationController < Sinatra::Base
   end
   #check that commits are working
 
+  #add helper methods
+  helpers do
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session [:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
+
 end
--- app/models/racecar.rb
+++ app/models/racecar.rb
@@ -1,3 +1,3 @@
 class Racecar < ActiveRecord::Base
-    belongs_to :owner
+    belongs_to :user
 end
