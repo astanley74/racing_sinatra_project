45d759af050a1bace55d4af1cf3b9b855083b19e
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-10-20 23:19:23 -0400

    mapped out part of racecars_controller.rb

--- app/controllers/racecars_controller.rb
+++ app/controllers/racecars_controller.rb
@@ -1,19 +1,34 @@
 class RacecarsController < ApplicationController
 
     get '/racecars' do
-        @racecars = Racecar.all
-        #show all racecars
+        #if not logged in
+            #redirect to login page
+        #else
+            #render users racecars
+        #end
     end
 
     get '/racecars/new' do
-        #route to form where racecar can be created
-        erb :'racecars/new'
+        #if user is logged in
+            erb :'racecars/new'
+        #else
+            #redirect to login page
+        #end
     end
 
     post '/racecars' do
         #create racecar and then redirect to page to show user's racecars
-        @racecar = Racecar.create(:car_name => params[:car_name], :number => params[:number], :driver => params[:driver], :driver_bio => params[:driver_bio])
-        erb :'racecars/show'
+        #if user is logged in
+            #if params[:car_name] == "" || params[:number] == "" || params[:driver] == "" || params[:driver_bio] == ""
+                #create racecar and redirect to users page where all racecars are listed
+                @racecar = Racecar.create(:car_name => params[:car_name], :number => params[:number], :driver => params[:driver], :driver_bio => params[:driver_bio])
+                erb :'racecars/show'
+            #else
+                #redirect to '/racecars/new'
+            #end
+        #else
+            #redirect to login
+        #end
     end
 
 
